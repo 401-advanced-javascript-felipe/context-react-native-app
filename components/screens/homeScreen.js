75f8e8a4194619a9b38b6958bf985261e2d271f1
Chloie Parsons <chloieparsons@gmail.com>
2019-07-23 09:56:12 -0700

    Felipe and Chloie working on getting logo in and home screen styling as expected.

--- components/screens/homeScreen.js
+++ components/screens/homeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Button, StyleSheet, TextInput } from 'react-native';
+import { View, Button, StyleSheet, TextInput, Image } from 'react-native';
 import { VibesContext } from '../context/vibesContext';
 
 export default class Page1 extends React.Component {
@@ -17,14 +17,17 @@ export default class Page1 extends React.Component {
         {values => {
           return (
             <View style={styles.container}>
+              <Image style={styles.logo} source={require('../../assets/vibes.png')}
+              />
               <TextInput
+                style={styles.inputField}
                 placeholder="Type your name"
                 value={values.playerName}
                 onChangeText={playerName =>
                   values.handleChange('playerName', playerName)
                 }
               />
-              <Button onPress={this._navigate} title="I want to play a game" />
+              <Button style={styles.play} onPress={this._navigate} title="I want to play a game" />
             </View>
           );
         }}
@@ -38,5 +41,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+
+  logo: {
+    marginBottom: 50,
+  },
+
+  inputField: {
+    marginBottom: 50,
+  },
+
+  play: {
+    marginBottom: 400,
+    
   },
 });
